712735e41880d35a3e4ef33459ef43500bb9aee6
计算机1733吴成杰 <48464503+2117726067@users.noreply.github.com>
2019-05-03 23:08:26 +0800

    Add files via upload

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,16 +4,16 @@ const app = getApp()
 
 Page({
   data: {
-    imgUrls: [
-      '/images/swiper01.jpg',
-      '/images/swiper02.jpg',
-      '/images/swiper03.jpg'
-    ],
-    indicatorDots: true,
-    autoplay: true,
-    interval: 2000,
-    duration: 1000,
-    circular:true,
+
+  },
+
+
+  //测试代码开始
+ 
+ //测试代码结束
+
+  onLoad: function () {
     
-  }
+  },
+
 })
